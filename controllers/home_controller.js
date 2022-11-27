3d7ab0c4f5bff159caa37583c9ed018e6ec4dafd
Zainab <zainabp491@gmail.com>
2022-11-27 19:49:26 +0530

    add a controller and router to r home

---
+++ controllers/home_controller.js
@@ -0,0 +1,9 @@
+module.exports.home=function(req,res){
+    return res.end('<h1>Express is up for codial!</h1');
+};
+
+//module.exports.actionName = function(req,res){};
+
+// module.exports.profile=function(req,res){
+//     return res.end('<h1>Cotrollers->home_cotroller->Profile</h1> ');
+// };
